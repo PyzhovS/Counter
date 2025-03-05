ccb1ae060fd4c48bf368ee6879ff27491300a774
Sergey <pyzhovs.kz@yandex.kz>
2025-03-05 22:19:01 +0500

    все исправлено

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -15,51 +15,56 @@ class ViewController: UIViewController {
         textView.text = "«История изменений:»"
         textView.isEditable = false
         textView.isScrollEnabled = true
-        // Do any additional setup after loading the view.
-        
     }
-    
-    @IBOutlet weak var textView: UITextView!
-    
-    @IBAction func minButton(_ sender: Any) {
-        if result > 0 {
-            result -= 1
-            textHistiry(text: "значение изменено на -1")
-        } else {
-            result = 0
-            textHistiry(text: "попытка уменьшить значение счётчика ниже 0")
-        }
-        labelText()
-    }
-    
-    @IBAction func maxButton(_ sender: Any) {
-        result += 1
-        labelText()
-        textHistiry(text: "значение изменено на +1")
-        
-    }
-    @IBAction func reset(_ sender: Any) {
-        result = 0
-        labelText()
-        textHistiry(text: "значение cброшено")
-    }
-    @IBOutlet weak var label: UILabel!
-    
-    var result = 0
-    
-    private func labelText() {
-        label.text = " «Значение счётчика: \(result) »"
-    }
-   
-    private func textHistiry(text:String) {
-        textView.text.append("\n" + "«\([dateF.string(from: Date())]):\(text)»")
-    }
-    
     private let dateF: DateFormatter = {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd HH-mm-ss"
         return df
     }()
+    
+    private var result = 0
+    
+    private func labelText() {
+        label.text = " «Значение счётчика: \(result) »"
+    }
+    
+    private func textHistory(text:String) {
+        textView.text.append("\n" + "«\([dateF.string(from: Date())]):\(text)»")
+    }
+    
+    @IBOutlet private weak var textView: UITextView!
+    
+    @IBAction private func minButton(_ sender: Any) {
+        if result > 0 {
+            result -= 1
+            textHistory(text: "значение изменено на -1")
+        } else {
+            result = 0
+            textHistory(text: "попытка уменьшить значение счётчика ниже 0")
+        }
+        labelText()
+    }
+    
+    @IBAction private func maxButton(_ sender: Any) {
+        result += 1
+        labelText()
+        textHistory(text: "значение изменено на +1")
+    
+    }
+    @IBAction private func reset(_ sender: Any) {
+        result = 0
+        labelText()
+        textHistory(text: "значение cброшено")
+    }
+    @IBOutlet private weak var label: UILabel!
+    
+    
+    
+    
+   
+    
+    
+   
     
     
 }
